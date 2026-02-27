4962ba49d29649bd3175ea9959f015bfb863cb37
PaulManriquez <paulmanriquezengineer@gmail.com>
2026-02-26 22:18:25 -0600

    Adding first version of admin page

---
+++ src/main/java/com/demoApp/demoApp/controller/AdminPageController.java
@@ -0,0 +1,16 @@
+package com.demoApp.demoApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminPageController {
+
+    @GetMapping("/")
+    public String showAdminHome(){
+        return "administration/index";
+    }
+
+}
